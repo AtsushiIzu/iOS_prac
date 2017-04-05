53fc3e31232a8d5086a8d3e5b3603fd86d6d6c59
AtsushiIzu <atsushi.izu@fuller.co.jp>
2017-04-05 10:51:43 +0900

    add FoodTracker

---
+++ FoodTracker/FoodTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by 伊津 惇 on 2017/04/05.
+//  Copyright © 2017年 Atsushi Izu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
